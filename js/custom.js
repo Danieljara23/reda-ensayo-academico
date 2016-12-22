675dd7ab1105e35da18ae1bf4a656aa04491853a
Danieljara23 <daniel.jaramillog23@udea.edu.co>
2016-12-22 16:51:41 -0500

    First book content is ready with styles an their respective contents. Also javascript and css are ok

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,22 @@ $(document).ready( function() {
   $(".pit-thumbnail").click(function(){
     var index = $(".pit-thumbnail").index(this);
     $(".pit-thumbnails-group").hide();
-    console.log("index es="+index)
     if (index == 0) {
       $(".pit-book").css("display","block");
     }
   })
+
+  $(".pit-book-toc p").on('click', function(){
+
+    $indexPage = $(".pit-book-toc p").index(this);
+
+    $(".pit-book-toc p.current").removeClass(" current");
+    $(this).addClass("current");
+
+    current = document.getElementsByClassName("pit-book-page current");
+    $(current).removeClass("current");
+    active = ".pit-book-page:nth-of-type("+($indexPage+1)+")";
+    $(active).addClass("current");
+  })
+
 });
